34d31929aef3ca1ee33c238b0c2d3319a2c307eb
andresmonc <jaimeandresmonc@gmail.com>
2020-09-24 11:29:41 -0500

    simple working terminal game

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,32 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("hi!");
+    PrintLine(TEXT("Welcome to Bull Cow Game"));
+    SetupGame();
+    int32 WordLength = HiddenWord.Len();
+    PrintLine(TEXT("Please guess the 4 letter word:")); // word length is a magic number
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (Input.Equals(HiddenWord))
+    {
+        PrintLine(TEXT("You have won!"));
+    }
+    else
+    {
+        if (Input.Len() == HiddenWord.Len())
+        {
+            PrintLine(TEXT("The hidden word is 4 letters"));
+        }
+        PrintLine(TEXT("You have lost.."));
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("self");
+    Lives = 3;
 }
