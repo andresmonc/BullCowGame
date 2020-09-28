abfb7674be55bdfd102655ffb22fb57d4696c002
andresmonc <jaimeandresmonc@gmail.com>
2020-09-28 18:31:44 -0500

    added word list

---
+++ Source/BullCowGame/HiddenWordList.h
@@ -0,0 +1,10 @@
+#include "CoreMinimal.h"
+
+const TArray<FString> Words =
+    {
+        TEXT("heck"),
+        TEXT("smelt"),
+        TEXT("wreck"),
+        TEXT("keys"),
+        TEXT("felt"),
+};
